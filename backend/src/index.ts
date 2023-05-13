7b401ab82296a263e51bbc66ae347c15e8ba9fd4
Max Jänicke <max.jaenicke@gmx.de>
2023-05-13 13:19:33 +0200

    Add cors to backend endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,11 @@ const port = 3000;
 const urlPomodoros = '/pomodoros';
 
 app.use(express.json());
+app.use(function (_req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const pool = new Pool({
   user: 'user',
